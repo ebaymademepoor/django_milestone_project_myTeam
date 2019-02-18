1d930dddf6be3e3cc460feedd61c04f1a479b05b
Giancarlo Fiorletta <caianroma@yahoo.co.uk>
2019-02-18 13:25:35 +0000

    Prep settings and requirements files for deployment

--- myTeam/settings.py
+++ myTeam/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 try:
     import env
     development = True
@@ -81,12 +82,14 @@ WSGI_APPLICATION = 'myTeam.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+DATABASES = {"default" : dj_database_url.parse('postgres://drjdzrclvhsfvh:cacb8fd45e352940accb8016a500ed871f4ecc8e4e66cc6a0210a8312807bbcf@ec2-176-34-113-195.eu-west-1.compute.amazonaws.com:5432/dakmvht49m6plj')}
 
 
 # Password validation

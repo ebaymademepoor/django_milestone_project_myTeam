e3707eba37da716183d48bc136bd79651ca5bfda
Giancarlo Fiorletta <caianroma@yahoo.co.uk>
2019-02-18 14:10:36 +0000

    Removed default secret key

--- myTeam/settings.py
+++ myTeam/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'u$*&2)7f0qe6c$lns=_n13nm1#h5usqalzawbjbnr#n0fd90e3'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development

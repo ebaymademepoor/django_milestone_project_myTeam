4d4afd42e6f4b1d84f99d6d86bfac6560cb2ff3e
Giancarlo Fiorletta <caianroma@yahoo.co.uk>
2019-02-18 13:54:03 +0000

    Implemented environ specific values for database

--- myTeam/settings.py
+++ myTeam/settings.py
@@ -82,14 +82,15 @@ WSGI_APPLICATION = 'myTeam.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-DATABASES = {"default" : dj_database_url.parse(os.environ.get('DATABASE_URL'))}
+if development:
+    DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+else:
+    DATABASES = {"default" : dj_database_url.parse(os.environ.get('DATABASE_URL'))}
 
 
 # Password validation

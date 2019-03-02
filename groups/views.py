bd09f780a31ca395d53029381e1d48bc1e7c81a0
Giancarlo Fiorletta <caianroma@yahoo.co.uk>
2019-03-02 12:18:12 +0000

    Added icons to nav bar contents, more minor styling changes and added some extra developer notes

--- groups/views.py
+++ groups/views.py
@@ -17,15 +17,15 @@ def group_select(request):
     or join new ones
     """
     
-    # Query to retrieve group information --------------------------------------
+    # Query to retrieve group information
+    
     my_profile = UserProfileData.objects.get(email=request.user.email)
     my_groups = my_profile.group_set.all()
     
     # Query to retrieve all users from a particular group
     Carlo = Group.objects.get(group_name="Carlo")
+    
     print(Carlo.users.all())
-    
-    
     
     create_group_form = CreateGroupForm()
     
@@ -39,31 +39,28 @@ def create_group(request):
         
         if request.method == "POST":
             
+            # Create data for new group using request.post details
+            
             new_group_data = {}
             new_group_data["password"] = request.POST["password"]
             new_group_data["password2"] = request.POST["password2"]
             new_group_data["creator"] = users_profile_data.username
             new_group_data["group_name"] = request.POST["group_name"]
             
+            
+            # Feed data into model form and create Group if valid
+            
             create_group_form = CreateGroupForm(new_group_data)
             
             if create_group_form.is_valid():
                 new_group_object = create_group_form.save()
                 
+                # Add this user (the creator) to the group
+                
                 new_group_object.users.add(users_profile_data)
                 new_group_object.save()
                 
-                # new_group_member_details = {}
-                # new_group_member_details["group_id"] = no_of_groups
-                # new_group_member_details["user_id"] = user.pk
-                # new_group_member_details["admin"] = True
-                
-                # new_group_member_form = AddNewGroupMemberForm(new_group_member_details)
-                
-                # if new_group_member_form.is_valid():
-                #     new_group_member_form.save()
-                #     new_group_member_form.profiles.add(user)
-                
+
                 messages.success(request, "{} has been created as your new group!".format(request.POST["group_name"]))
                 return redirect(reverse('group-select'))
                 

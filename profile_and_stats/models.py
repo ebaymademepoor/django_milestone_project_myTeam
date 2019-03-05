d3c57ca72d5908fce25461a644b5b98d501b159c
Giancarlo Fiorletta <caianroma@yahoo.co.uk>
2019-03-05 14:08:09 +0000

    Added new model to store attribute ratings - AttributeRating

--- profile_and_stats/models.py
+++ profile_and_stats/models.py
@@ -24,3 +24,19 @@ class UserProfileData(models.Model):
     
     def __str__(self):
         return self.email
+        
+class AttributeRating(models.Model):
+    player_rated = models.ForeignKey(UserProfileData, on_delete=models.CASCADE, related_name="player_rated")
+    rated_by = models.ForeignKey(UserProfileData, on_delete=models.CASCADE, related_name="rated_by")
+    gk_score = models.DecimalField(max_digits=2, decimal_places=0, default=0, null=False, blank=False)
+    def_score = models.DecimalField(max_digits=2, decimal_places=0, default=0, null=False, blank=False)
+    passing_score = models.DecimalField(max_digits=2, decimal_places=0, default=0, null=False, blank=False)
+    finishing_score = models.DecimalField(max_digits=2, decimal_places=0, default=0, null=False, blank=False)
+    movement_score = models.DecimalField(max_digits=2, decimal_places=0, default=0, null=False, blank=False)
+    
+    def outfield_score(self):
+        scores = [self.gk_score, self.def_score, self.passing_score, self.finishing_score, self.movement_score]
+        len_scores = len(scores)
+        total_scores = sum(scores)
+        
+        return total_scores / len_scores

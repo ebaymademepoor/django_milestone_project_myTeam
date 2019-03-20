2a315608c228ead94b57ba8fd236bcbf3fb31e6d
Giancarlo Fiorletta <caianroma@yahoo.co.uk>
2019-03-20 15:09:57 +0000

    Updated team generation page to pull trhough more stats and information about each player ready for team creation

--- team_gen/views.py
+++ team_gen/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from groups.models import Group
 from matches.models import AvailabilityTable
+from profile_and_stats.models import AttributeRating
+from django.db.models import Avg
 from django.db import connection
 
 # Create your views here.
@@ -10,7 +12,15 @@ from django.db import connection
 def team_gen_settings(request, matchid):
     
     group_data = Group.objects.get(linked_group__pk=matchid)
-    
     avail_data = AvailabilityTable.objects.filter(matchID=matchid)
+    rating_data = AttributeRating.objects.filter(
+        player_rated__my_group__pk=group_data.pk).values(
+            'player_rated').annotate(
+                avg_gk=Avg('gk_score'), 
+                avg_def=Avg('def_score'), 
+                avg_move=Avg('movement_score'), 
+                avg_pass=Avg('passing_score'),
+                avg_fin=Avg('finishing_score'),
+                avg_out=(Avg('def_score')+Avg('movement_score')+Avg('passing_score')+Avg('finishing_score'))/4 )
     
-    return render(request, 'gen_settings.html', {"avail_data" : avail_data, "group_data" : group_data})
+    return render(request, 'gen_settings.html', {"avail_data" : avail_data, "group_data" : group_data, "rating_data": rating_data})
